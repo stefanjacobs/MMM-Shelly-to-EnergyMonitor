54c1d98eea52ea1f29d7ea2c3e21b5950896b4ca
Stefan Jacobs <stefan.jacobs@deutschebahn.com>
2022-10-25 10:29:27 +0200

    Update for Shelly monitoring to energy monitor magic mirror module

--- MMM-Shelly-to-EnergyMonitor.js
+++ MMM-Shelly-to-EnergyMonitor.js
@@ -1,7 +1,20 @@
 Module.register("MMM-Shelly-to-EnergyMonitor",{
 
+	data: {
+		solar_now: "--",
+		solar_today: "--",
+		solar_units_today: "--",
+
+		grid_consumption_now: "--",
+		grid_consumption_today: "--",
+		grid_consumption_units_today: "--",
+		grid_returned_today: "--",
+		grid_returned_units_today: "--"
+	},
+
 	start: function() {
 		var self = this;
+
 
 		// little helper to immediately execute func before setting the timer
 		function setIntervalImmediately(func, interval) {
@@ -13,10 +26,21 @@ Module.register("MMM-Shelly-to-EnergyMonitor",{
 		setIntervalImmediately(function() {
 			var payload = {
 				uri: self.config.grid_em3_uri,
-				timeout: self.config.grid_em3_timeout
+				timeout: self.config.timeout
 			}
 			self.sendSocketNotification("GetShellyGridToEnergyMonitor", payload)
-		}, this.config.grid_em3_refreshInterval);
+		}, self.config.grid_em3_refreshInterval);
+
+		setIntervalImmediately(function() {
+			var payload = {
+				uri: self.config.grid_em3_uri_cloud,
+				timeout: self.config.timeout,
+				device_id: self.config.grid_em3_device_id,
+				auth_key: self.config.authkey
+			}
+			self.sendSocketNotification("GetShellyGridCloud", payload)
+		}, self.config.grid_em3_refreshInterval_cloud);
+
 
 		// set solar timer and call immediately
 		setIntervalImmediately(function() {
@@ -26,16 +50,35 @@ Module.register("MMM-Shelly-to-EnergyMonitor",{
 				solar_pm_min_watts: self.config.solar_pm_min_watts
 			}
 			self.sendSocketNotification("GetShellySolarToEnergyMonitor", payload)
-		}, this.config.solar_pm_refreshInterval);
+		}, self.config.solar_pm_refreshInterval);
+
+		setIntervalImmediately(function() {
+			var payload = {
+				uri: self.config.solar_pm_uri_cloud,
+				timeout: self.config.timeout,
+				device_id: self.config.solar_pm_device_id,
+				auth_key: self.config.authkey
+			}
+			self.sendSocketNotification("GetShellySolarCloud", payload)
+		}, self.config.solar_pm_refreshInterval_cloud);
 	},
 
 	// when any method returns, notify the energy-monitor
-	socketNotificationReceived: function(notification, payload) {
+	socketNotificationReceived: function (notification, payload) {
+		var self = this;
+
 		if (notification == "ShellySolarToEnergyMonitor") {
-			this.sendNotification("MMM-EnergyMonitor_SOLAR_POWER_UPDATE", payload.power);
+			self.sendNotification("MMM-EnergyMonitor_SOLAR_POWER_UPDATE", payload.power);
 		}
 		if (notification == "ShellyGridToEnergyMonitor") {
-			this.sendNotification("MMM-EnergyMonitor_GRID_POWER_UPDATE", -payload.power);
+			self.sendNotification("MMM-EnergyMonitor_GRID_POWER_UPDATE", -payload.power);
+		}
+
+		if (notification == "ShellySolarCloud") {
+			// console.log("Got cloud data for solar");
+		}
+		if (notification == "ShellyGridCloud") {
+			// console.log("Got cloud data for em3 grid");
 		}
 	}
 
--- node_helper.js
+++ node_helper.js
@@ -1,43 +1,139 @@
-var NodeHelper = require("node_helper");
+const NodeHelper = require('node_helper');
+const axios = require("axios");
+const agent = require("agentkeepalive");
 
-const request = require('request');
+axios.defaults.timeout = 10000;
+
 
 module.exports = NodeHelper.create({
 
-	start: function() {
-
+    start: function() {
+		var self = this;
+		self.solarLanHttpAgent = new agent({ maxSockets: 20, maxFreeSockets: 10, timeout: 60000, freeSocketTimeout: 30000 });
+		self.gridLanHttpAgent = new agent({ maxSockets: 20, maxFreeSockets: 10, timeout: 60000, freeSocketTimeout: 30000 });
+		self.solarCloudHttpAgent = new agent({ maxSockets: 20, maxFreeSockets: 10, timeout: 120000, freeSocketTimeout: 30000 });
+		self.gridCloudHttpAgent = new agent({ maxSockets: 20, maxFreeSockets: 10, timeout: 120000, freeSocketTimeout: 30000 });
 	},
 
+	getPrintDate: function() {
+		var currentdate = new Date();
+		var options = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
+		return new Intl.DateTimeFormat('de', options).format(currentdate);
+	},
+
+	getCloudDate: function() {
+		var currentdate = new Date();
+		var month = '' + (currentdate.getMonth() + 1);
+		var day = '' + currentdate.getDate();
+		var year = currentdate.getFullYear();
+		if (month.length < 2)
+			month = '0' + month;
+		if (day.length < 2)
+			day = '0' + day;
+		// return [year, month, day].join('-') + "%20%2000%3A00%3A00";
+		return [year, month, day].join('-') + "  00:00:00";
+	},
+
+	getFilterDate: function() {
+		var currentdate = new Date();
+		var month = '' + (currentdate.getMonth() + 1);
+		var day = '' + currentdate.getDate();
+		var year = currentdate.getFullYear();
+		if (month.length < 2)
+			month = '0' + month;
+		if (day.length < 2)
+			day = '0' + day;
+		return [year, month, day].join('-');
+	},
 
 	// Frontend module pings the node helper to fetch data from Shelly PM
 	socketNotificationReceived: function (notification, payload) {
-		self = this;
+		var self = this;
 
 		if (notification == "GetShellySolarToEnergyMonitor"){
-			request(payload.uri, {json: true, timeout: payload.timeout}, (err, res, body) => {
-				if (err) { return console.log(err); }
-				var power = body['switch:0'].apower
-				// the workaround such that the line is rendered and shown as data is available
-				if (power <= payload.solar_pm_min_watts) {
-					power = 0.0
+			axios.get(payload.uri, {timeout: payload.timeout, httpAgent: self.solarLanHttpAgent} )
+			.then(response => {
+				var result={
+					power: response.data['switch:0'].apower
 				}
-
-				payload_solar={
-					power: power
+				if (result.power <= payload.solar_pm_min_watts) {
+					result.power = 0.0
 				}
-				self.sendSocketNotification('ShellySolarToEnergyMonitor', payload_solar);
-			});
+				console.log(result);
+				self.sendSocketNotification('ShellySolarToEnergyMonitor', result);
+			})
+			.catch(error => {
+				return console.log(error);
+			})
 		}
 
 		if (notification == "GetShellyGridToEnergyMonitor"){
-			request(payload.uri, {json: true, timeout: payload.timeout}, (err, res, body) => {
-				if (err) { return console.log(err); }
-
-				payload_grid={
-					power: body.total_power
+			axios.get(payload.uri, {timeout: payload.timeout, httpAgent: self.gridLanHttpAgent} )
+			.then(response => {
+				var result = {
+					power: response.data.total_power
 				}
-				self.sendSocketNotification('ShellyGridToEnergyMonitor', payload_grid);
-			});
+				console.log(result);
+				self.sendSocketNotification('ShellyGridToEnergyMonitor', result);
+			})
+			.catch(error => {
+				return console.log(error);
+			})
+
 		}
+
+		if (notification == "GetShellySolarCloud") {
+			// curl -d "channel=0&date_range=day&date=20221015&id=485519c9b0a5&auth_key=MTIxZTUxdWlk356CAD19444095CAF7832666D85E367AB2454479AA703DED4E0A86313C3A3519481AE4C7E22CF538" -H "Content-Type: application/x-www-form-urlencoded" -X POST "https://shelly-39-eu.shelly.cloud/statistics/relay/consumption"
+			axios.post( payload.uri,
+				new URLSearchParams({
+					'channel': '0',
+					'date_range': 'day',
+					'date_from': self.getCloudDate(),
+					'id': payload.device_id,
+					'auth_key': payload.auth_key
+				}),
+				{timeout: payload.timeout, httpAgent: self.solarCloudHttpAgent})
+			.then(response => {
+				const {data} = response.data;
+				var result = {
+					power: data.total,
+					power_unit: data.units.consumption
+				}
+				console.log(result);
+				self.sendSocketNotification("ShellySolarCloud", result);
+			})
+			.catch(error => {
+				return console.log(error)
+			})
+		}
+
+		if (notification == "GetShellyGridCloud") {
+			// curl -d "channel=0&date_range=day&date_from=2022-10-15%20%2000%3A00%3A00&id=485519c9b0a5&auth_key=MTIxZTUxdWlk356CAD19444095CAF7832666D85E367AB2454479AA703DED4E0A86313C3A3519481AE4C7E22CF538" -H "Content-Type: application/x-www-form-urlencoded" -X POST "https://shelly-39-eu.shelly.cloud/statistics/emeter/consumption3p"
+			axios.post( payload.uri,
+				new URLSearchParams({
+					'channel': '0',
+					'date_range': 'day',
+					'date_from': self.getCloudDate(),
+					'id': payload.device_id,
+					'auth_key': payload.auth_key
+				}),
+				{timeout: payload.timeout, httpAgent: self.gridCloudHttpAgent})
+			.then(response => {
+				const {data} = response.data;
+				result = {
+					power: data.total_c3,
+					power_unit: data.units.consumption,
+					returned: data.total_r3,
+					returned_unit: data.units.reversed
+				}
+				console.log(result);
+				self.sendSocketNotification("ShellyGridCloud", result);
+			})
+			.catch(error => {
+				return console.log(error)
+			})
+		}
+
 	}
 });
+
